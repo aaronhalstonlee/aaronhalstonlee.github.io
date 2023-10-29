669966909a953c5b19eee9ac62092b62eaf621bd
aaronhalstonlee <aaronhalstonlee@gmail.com>
2023-10-28 23:24:32 -0700

    begin translating jquery to js

--- projects/weather/script.js
+++ projects/weather/script.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
     //globals
     var appid = "c3494159ec59eb5abb08b856c55a5830";
     var lat = '', lon = '';
@@ -6,44 +11,53 @@ $(document).ready(function(){
     var temp;
     
     //get location
-    function getWeather(){
-        //get coordinates
-        function locate(spot){
+    let getWeather = () => {
+        //get coords
+        let locate = (spot) => {
             lat = spot.coords.latitude.toFixed(4);
             lon = spot.coords.longitude.toFixed(4);
             weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=${appid}`;
         };
-        
+
         //when not able to locate
-        function err(){
-            alert("sorry, I can't find you");
-        }
-        
+        let err = () => {
+            alert("Oops... I'm having trouble finding you...")
+        };
+
         //location call
         navigator.geolocation.getCurrentPosition(locate, err);
-        
+
         //api call for weather data
-        function weatherData(){
-            $.getJSON(weatherURL, function(data){
-                temp = data.list[0].main.temp;
-                var weatherDescript = data.list[0].weather[0].description;
-                var weatherIcon = data.list[0].weather[0].icon;
-                $('#location').html(data.city.name)
-                $('#weather').html(`<span id="temperature">${(9/5*(temp-273)+32).toFixed(1)}</span><span id="unit">°F</span><br><img style="height: 75px;" src="https://openweathermap.org/img/w/${weatherIcon}.png"> ${weatherDescript}`);
-            });
+        let weatherData = () => {
+            let xhttp = new XMLHttpRequest();
+            xhttp.open('GET', weatherURL);
+            xhttp.send();
+
+            xhttp.onload = () => {
+                if(xhttp.status != 200){
+                    alert("http failure, i guess... ? : " + xhttp.status);
+                    return;
+                }
+
+                console.log(xhttp.response);
+            }
         };
         setTimeout(weatherData, 500);
     };
     getWeather();
-    
+
     //change temp units
-    $('#today').click(function(){
-        if ($('#unit').html() == "°F"){
-            $('#unit').html("°C");
-            $('#temperature').html((temp - 273).toFixed(1));
-        } else if ($('#unit').html() == "°C"){
-            $('#unit').html("°F");
-            $('#temperature').html((9/5*(temp - 273)+32).toFixed(1));
-        }
+    let today = document.getElementById("today");
+    today.addEventListener("click", () => {
+        let unit = document.getElementById("unit");
+        let temp = document.getElementById("temperature");
+
+        if (unit.innerHTML == "°F"){
+            unit.innerHTML = "°C";
+            temp.innerHTML = (temp - 273).toFixed(1);
+        } else if (unit.innerHTML == "°C"){
+            unit.innerHTML = "°F";
+            temp.innerHTML = (9/5*(temp -273) + 32).toFixed(1)
+        };
     });
 });
