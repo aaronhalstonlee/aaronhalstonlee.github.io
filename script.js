2fe27e5a55408d49b5a0338f91c56e41edbea624
aaron <33557557+aaronhalstonlee@users.noreply.github.com>
2026-03-21 11:30:47 -0700

    Create script.js
    separate script to own file

---
+++ script.js
@@ -0,0 +1,10 @@
+const cards = document.querySelectorAll('.card');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach((entry, i) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => entry.target.classList.add('visible'), i * 100);
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.1 });
+cards.forEach(c => observer.observe(c));
